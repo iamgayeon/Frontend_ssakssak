4c14d052ce358d95460c5a939e103f1ebe1631ae
minju <betaa0528@gmail.com>
2024-09-22 23:35:41 +0900

    부트스트랩, apexChart 의존성 추가 / 박민주

--- src/main.js
+++ src/main.js
@@ -1,25 +1,27 @@
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue-3/dist/bootstrap-vue-3.css";
+import "vue-awesome-paginate/dist/style.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "./assets/main.css";
 
-import 'bootstrap/dist/css/bootstrap.css';
-import 'vue-awesome-paginate/dist/style.css';
-import './assets/main.css';
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import VueAwesomePaginate from "vue-awesome-paginate";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-
-import { createApp } from 'vue';
-import { createPinia } from 'pinia';
-import VueAwesomePaginate from 'vue-awesome-paginate';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-
-import App from './App.vue';
-import router from './router';
+import App from "./App.vue";
+import router from "./router";
+import VueApexCharts from "vue3-apexcharts";
 
 // 필요한 아이콘 라이브러리를 추가하세요
-import { faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faUser, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 const app = createApp(App);
 
 app.use(VueAwesomePaginate);
 app.use(createPinia());
+app.component("apexchart", VueApexCharts);
 app.use(router);
 
-app.mount('#app');
+app.mount("#app");
