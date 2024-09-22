8a002c4d10cd552d5739e1d568dc95681efc2e22
minju <betaa0528@gmail.com>
2024-09-22 23:36:13 +0900

    구현중인 페이지 보기위한 url설정 / 박민주

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,28 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import HomePage from '../pages/HomePage.vue';
-import authRotes from './auth';
-import boardRotes from './board';
-import travelRoutes from './travel';
-import galleryRoutes from './gallery';
+import { createRouter, createWebHistory } from "vue-router";
+import HomePage from "../pages/HomePage.vue";
+import authRotes from "./auth";
+import boardRotes from "./board";
+import travelRoutes from "./travel";
+import galleryRoutes from "./gallery";
+import StudentStockPage from "@/pages/student/stock/StudentStockPage.vue";
+import TeacherStockPage from "@/pages/teacher/stock/TeacherStockPage.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: HomePage,
+    },
+    {
+      path: "/student/stock",
+      name: "StudentStockPage",
+      component: StudentStockPage,
+    },
+    {
+      path: "/teacher/stock",
+      component: TeacherStockPage,
     },
     ...authRotes,
     ...boardRotes,
