ba96b5d6d3184f1630b095b44f55419cc252fc94
minjoo <betaa0528@gmail.com>
2024-09-24 19:56:19 +0900

    #6 router reward 추가 / 박민주 / 20240924_1944

--- src/router/student.js
+++ src/router/student.js
@@ -1,13 +1,21 @@
 export default [
-    {
-      path: '/student/bank',
-      name: 'StudentBank',
-      component: () => import('../pages/student/bank/StudentBank.vue'),
-    },
-    {
-        path: '/student/event',
-        name: 'StudentEvent',
-        component: () => import('../pages/student/event/StudentEvent.vue'),
-      },
-  ];
-  
+  {
+    path: '/student/bank',
+    name: 'StudentBank',
+    component: () => import('../pages/student/bank/StudentBank.vue'),
+  },
+  {
+    path: '/student/event',
+    name: 'StudentEvent',
+    component: () => import('../pages/student/event/StudentEvent.vue'),
+  },
+  {
+    path: '/student/store',
+    name: 'StudentStore',
+    component: () => import('../pages/student/store/StudentStorePage.vue'),
+  },
+  {
+    path: '/reward',
+    component: () => import('../pages/teacher/reward/TeacherRewardPage.vue')
+  }
+];
