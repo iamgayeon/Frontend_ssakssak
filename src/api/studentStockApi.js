d6247dbc5d1cf4d0ab2f34a7cb6e390e0cb03c88
minju <betaa0528@gmail.com>
2024-10-09 23:56:54 +0900

    #33 - 실질적으로 api를 호출하는 곳이다. 이곳에 api를 호출하는 함수를 작성하는것을 잊지 말자 / 박민주

--- src/api/studentStockApi.js
+++ src/api/studentStockApi.js
@@ -1,28 +1,32 @@
-import api from '@/api';
+import api from "@/api";
 
-const BASE_URL = '/api/student/stock';
+const BASE_URL = "/api/student/stock";
 
-const headers = { 'Content-Type': 'multipart/form-data' };
-
+const headers = { "Content-Type": "multipart/form-data" };
 
 export default {
-    async getNewsList() {
-        const {data} = await api.get(`${BASE_URL}/news`);
-        console.log('NEWS LIST GET', data);
-        return data;
-    },
+  async getNewsList() {
+    const { data } = await api.get(`${BASE_URL}/news`);
+    console.log("NEWS LIST GET", data);
+    return data;
+  },
 
-    async updateNewsStatus(newsId) {
-        try {
-            const response = await api.put(`${BASE_URL}/news/${newsId}/status`, {
-                status: 'n',
-            });
-            console.log('News status updated:', response.data);
-            return response.data;
-        } catch (error) {
-            console.error('Error updating news status:', error);
-            throw error;
-        }
+  async updateNewsStatus(newsId) {
+    try {
+      const response = await api.put(`${BASE_URL}/news/${newsId}/status`, {
+        status: "n",
+      });
+      console.log("News status updated:", response.data);
+      return response.data;
+    } catch (error) {
+      console.error("Error updating news status:", error);
+      throw error;
     }
-};
+  },
 
+  async getMyStock() {
+    const { data } = await api.get(`${BASE_URL}/my-stock`);
+    console.log("get my stock: ", data);
+    return data;
+  },
+};
