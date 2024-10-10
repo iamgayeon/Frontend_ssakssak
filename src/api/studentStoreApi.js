8b4be0137a6e004285b2ac872a10590bbcd6c5c0
minju <betaa0528@gmail.com>
2024-10-11 00:43:01 +0900

    #55 - 쿠폰을 구매하는 api를 작성하였다. / 박민주

--- src/api/studentStoreApi.js
+++ src/api/studentStoreApi.js
@@ -1,27 +1,37 @@
-import api from '@/api';
+import api from "@/api";
 
-const BASE_URL = '/api/student/coupon';
+const BASE_URL = "/api/student/coupon";
 
 export default {
-    async getCouponlist() {
-        try {
-            const { data } = await api.get(`${BASE_URL}/list`);
-            console.log('Couponlist info GET', data);
-            return data;
-        } catch (error) {
-            console.error('Failed to fetch coupon list:', error);
-            throw error;
-        }
-    },
+  async getCouponlist() {
+    try {
+      const { data } = await api.get(`${BASE_URL}/list`);
+      //   console.log("Couponlist info GET", data);
+      return data;
+    } catch (error) {
+      console.error("Failed to fetch coupon list:", error);
+      throw error;
+    }
+  },
 
-    async getCouponById(cpId) {
-        try {
-            const { data } = await api.get(`${BASE_URL}/list/${cpId}`);
-            console.log('Coupon info GET by ID', data);
-            return data;
-        } catch (error) {
-            console.error(`Failed to fetch coupon with id ${cpId}:`, error);
-            throw error;
-        }
-    },
+  async getCouponById(cpId) {
+    try {
+      const { data } = await api.get(`${BASE_URL}/list/${cpId}`);
+      //   console.log("Coupon info GET by ID", data);
+      return data;
+    } catch (error) {
+      console.error(`Failed to fetch coupon with id ${cpId}:`, error);
+      throw error;
+    }
+  },
+
+  async buyCoupon(request) {
+    try {
+      const response = await api.post(`${BASE_URL}/buy`, request.value);
+      return response;
+    } catch (error) {
+      console.error("Failed to buy coupon", error);
+      throw error;
+    }
+  },
 };
