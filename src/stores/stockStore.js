ba4b21c54c04c95b4224f655fabf779c412826f5
minju <betaa0528@gmail.com>
2024-10-11 00:34:44 +0900

    #55 - 주식에 대해서 약간 수정한 부분이 있어 커밋 / 박민주

--- src/stores/stockStore.js
+++ src/stores/stockStore.js
@@ -1,43 +1,41 @@
-import { defineStore } from 'pinia';
-import api from '@/api/studentStockApi';
+import { defineStore } from "pinia";
+import api from "@/api/studentStockApi";
 
-export const useStockStore = defineStore('stock', {
-    state: () => ({
-        myStock: {
-            stdId: '',
-            totalQuantity: '',
-            totalInvestment: '',
-            averagePrice: '',
-            currentValue: '',
-            profitLoss: '',
-            profitRate: '',
-            seed: ''
-        },
-        chartData: [],
-
-    }),
-    actions: {
-        async fetchMyStock() {
-            try {
-                const response = await api.getMyStock();
-                // console.log(response);
-                this.myStock = response;
-            } catch (error) {
-                console.error('Failed to fetch stock:', error);
-            }
-        },
-        updateMyStock(stockData) {
-            this.myStock = stockData;
-        },
-        async fetchChartData() {
-            try {
-                const response = await api.getChartData();
-                this.chartData = response;
-                // console.log("tq" , response);
-            } catch (error) {
-                console.error('Failed to fetch chart data:', error);
-            }
-        }
+export const useStockStore = defineStore("stock", {
+  state: () => ({
+    myStock: {
+      stdId: "",
+      totalQuantity: "",
+      totalInvestment: "",
+      averagePrice: "",
+      currentValue: "",
+      profitLoss: "",
+      profitRate: "",
+      seed: "",
     },
-    persist: true
+    chartData: [],
+  }),
+  actions: {
+    async fetchMyStock() {
+      try {
+        const response = await api.getMyStock();
+        // console.log(response);
+        this.myStock = response;
+      } catch (error) {
+        console.error("Failed to fetch stock:", error);
+      }
+    },
+    updateMyStock(stockData) {
+      this.myStock = stockData;
+    },
+    async fetchChartData() {
+      try {
+        const response = await api.getChartData();
+        this.chartData = response;
+      } catch (error) {
+        console.error("Failed to fetch chart data:", error);
+      }
+    },
+  },
+  persist: true,
 });
