f950b98749461a36880c46a546549eea46ccd431
minju <betaa0528@gmail.com>
2024-10-12 00:50:01 +0900

    #64 - 선생님 페이지에서 리워드 관리를 작성하였다. 리워드 지급 목록의 경우 페이징 처리를 완료 하였다.

--- src/stores/rewardStore.js
+++ src/stores/rewardStore.js
@@ -2,19 +2,40 @@ import { defineStore } from "pinia";
 import api from "@/api/teacherRewardApi";
 
 export const useRewardStore = defineStore("reward", {
-    state: () => ({        
-        rewardList: [],
-    }),
-    actions: {
-        async fetchRewardList() {
-            try {
-                const response = await api.getRewardList();
-                // console.log(response);
-                this.rewardList = response;
-            } catch (error) {
-                console.error("Failed to fetch reward:", error);
-            }
-        }
+  state: () => ({
+    rewardList: [],
+    rewardGiveList: [],
+    studentList: [],
+  }),
+  actions: {
+    async fetchRewardList() {
+      try {
+        const response = await api.getRewardList();
+        // console.log(response);
+        this.rewardList = response;
+      } catch (error) {
+        console.error("Failed to fetch reward:", error);
+        throw error;
+      }
     },
-    persist: true,
+    async fetchStudentList() {
+      try {
+        const response = await api.getStudentList();
+        this.studentList = response;
+      } catch (error) {
+        console.error("Failed to fetch studentList:", error);
+        throw error;
+      }
+    },
+    async fetchRewardGiveList() {
+      try {
+        const response = await api.getRewardGiveList();
+        this.rewardGiveList = response;
+      } catch (error) {
+        console.error("Failed to fetch rewardGiveList", error);
+        throw error;
+      }
+    },
+  },
+  persist: true,
 });
